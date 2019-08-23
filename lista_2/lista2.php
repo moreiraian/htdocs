b345b2188fc851d8fc51d609a6c54679a416e1a6
moreiraian <moreiraian@users.noreply.github.com>
2019-08-23 19:33:02 -0300

    ate a 3 feita

--- lista_2/lista2.php
+++ lista_2/lista2.php
@@ -11,8 +11,8 @@
                     <fieldset>
                         <form action="#" method="post">
                             <div class="form-group">
-                                <label for="id_nome"> Valor </label>
-                                <input type="number" class="form-control" 
+                                <label for="id_nome" > Valor </label>
+                                <input type="number" placeholder = "Questão 1" class="form-control" 
                                 required id="id_nome" name="valor1">
                             </div>
                           
@@ -28,9 +28,63 @@
                 echo "<br>" .($valor01*-1);
                 else
                 echo $valor01;
-        
+                ?>
+                <!-- 2 -->
+                <div class="container ">
+                    <fieldset>
+                        <form action="#" method="post">
+                            <div class="form-group">
+                                <label for="id_nome" > Valor </label>
+                                <input type="number" placeholder = "Questão 2" class="form-control" 
+                                required id="id_nome" name="valor2">
+                            </div>
+                          
+                            <button type="submit" class="btn btn-dark">Enviar</button>
+                        </form>
+                    </fieldset>
+                </div>
+                <br> 
+                <div class="container">
+                <?php 
+                $valor02 = isset($_POST['valor2']) ? $_POST ['valor2'] : 0;
+                if ($valor02%2 >0)
+                echo "ímpar" ;
+                else
+                echo "par";
+               
+                ?>
+                <!-- 3 -->
+                <div class="container ">
+                    <fieldset>
+                        <form action="#" method="post">
+                            <div class="form-group">
+                                <label for="id_nome" > Valor </label>
+                                <input type="number" placeholder = "Questão 3" class="form-control" 
+                                required id="id_nome" name="valor3">
+                            </div>
+                            <div class="form-group">
+                                <label for="id_nome" > Valor </label>
+                                <input type="number" placeholder = "Questão 3" class="form-control" 
+                                required id="id_nome" name="valor3a">
+                            </div>
+                          
+                            <button type="submit" class="btn btn-dark">Enviar</button>
+                        </form>
+                    </fieldset>
+                </div>
+                <br> 
+                <div class="container">
+                <?php 
                 
-                    ?>
+                if($valor03 = isset($_POST['valor3']) && ($_POST['valor3'] != 0) && ($_POST['valor3a'])){
+                $valor03 =$_POST["valor3"];
+                $valor03a =$_POST["valor3a"];
+                if ($valor03%$valor03a == 0)
+                echo "Múltiplos";
+                else
+                echo "Não são múltiplos" ;};
+                
+                ?>
          </div>
     </body>
     </html>
